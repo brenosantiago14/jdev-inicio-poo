eda975ffe4cf01f6073d8667003122a0ac8da7a1
Breno Santiago <brenosantiago14@gmail.com>
2022-03-02 14:03:35 -0500

    Criação do projeto e primeiro commit sem ajuda do curso. (Teste)

---
+++ primeiro-projeto-jdev/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,19 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno();
+		
+		Aluno aluno2 = new Aluno("João");
+		
+		Aluno aluno3 = new Aluno("Maria", 20);
+		
+		
+
+	}
+
+}
